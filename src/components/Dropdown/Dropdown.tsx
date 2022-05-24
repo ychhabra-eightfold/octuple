ab6376b2d3ca68eea630f28086e964741ff15668
Yash Raj Chhabra <ychhabra@eightfold.ai>
2022-05-24 18:53:06 +0530

    chore: fix dropdown

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -51,7 +51,7 @@ export const Dropdown: FC<DropdownProps> = ({
             );
         };
 
-    useOnClickOutside(refs.reference, toggle(false), visible);
+    useOnClickOutside(refs.floating, toggle(false), visible);
 
     useEffect(() => {
         onVisibleChange?.(visible);
